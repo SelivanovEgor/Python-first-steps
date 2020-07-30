8951450fcfcb71e96eae53752e929ba8b43e03d5
Beetle <egorselivanov0@gmail.com>
2020-07-30 19:46:03 +0300

    Second lesson(2)

--- Основы Python/2. Условные конструкции/chess.py
+++ Основы Python/2. Условные конструкции/chess.py
@@ -1,31 +1,35 @@
 print("Введите четыре числа от 1 до 8 каждое, задающие номер столбца и номер строки сначала для первой клетки, "
       "потом для второй клетки: ", sep="\n")
-x1 = int(input())
-y1 = int(input())
-x2 = int(input())
-y2 = int(input())
-if (1 <= x1 <= 8) and (1 <= y1 <= 8) and (1 <= x2 <= 8) and (1 <= y2 <= 8):
-    if x1 - 2 == x2:
-        if (y1 - 1 == y2) or (y1 + 1 == y2):
-            print("YES")
-        else:
-            print("NO")
-    elif x1 - 1 == x2:
-        if (y1 - 2 == y2) or (y1 + 2 == y2):
-            print("YES")
-        else:
-            print("NO")
-    elif x1 + 1 == x2:
-        if (y1 - 2 == y2) or (y1 + 2 == y2):
-            print("YES")
-        else:
-            print("NO")
-    elif x1 + 2 == x2:
-        if (y1 - 1 == y2) or (y1 + 1 == y2):
-            print("YES")
+try:
+    x1 = int(input())
+    y1 = int(input())
+    x2 = int(input())
+    y2 = int(input())
+except:
+    print("Невозможно обработать данные! Пожалуйста, введите данные, удовлетворяющие условиям!")
+else:
+    if (1 <= x1 <= 8) and (1 <= y1 <= 8) and (1 <= x2 <= 8) and (1 <= y2 <= 8):
+        if x1 - 2 == x2:
+            if (y1 - 1 == y2) or (y1 + 1 == y2):
+                print("YES")
+            else:
+                print("NO")
+        elif x1 - 1 == x2:
+            if (y1 - 2 == y2) or (y1 + 2 == y2):
+                print("YES")
+            else:
+                print("NO")
+        elif x1 + 1 == x2:
+            if (y1 - 2 == y2) or (y1 + 2 == y2):
+                print("YES")
+            else:
+                print("NO")
+        elif x1 + 2 == x2:
+            if (y1 - 1 == y2) or (y1 + 1 == y2):
+                print("YES")
+            else:
+                print("NO")
         else:
             print("NO")
     else:
-        print("NO")
-else:
-    print("Введены некорректные данные!")
+        print("Введены некорректные данные!")
--- Основы Python/2. Условные конструкции/chocolate.py
+++ Основы Python/2. Условные конструкции/chocolate.py
@@ -1,10 +1,17 @@
-n = int(input("Введите число n: "))
-m = int(input("Введите число m: "))
-k = int(input("Введите число k: "))
-if k > n * m:
-    print("NO")
+try:
+    n = int(input("Введите число n: "))
+    m = int(input("Введите число m: "))
+    k = int(input("Введите число k: "))
+except:
+    print("Вы ввели не числа!")
 else:
-    if (k % n == 0) or (k % m == 0):
-        print("YES")
-    else:
+    if k > n * m:
         print("NO")
+    else:
+        try:
+            if (k % n == 0) or (k % m == 0):
+                print("YES")
+            else:
+                print("NO")
+        except:
+            print("Введите корректные данные!")
